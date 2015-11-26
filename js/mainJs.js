466d79d4a4e110bd28db23bcaa55495eeba8f3d3
Chronyms <caspar.brenneisen@hotmail.ch>
2015-11-26 21:26:04 +0100

    minimal responsive Menu
    This is the minifyed version of responsive Menu

---
+++ js/mainJs.js
@@ -0,0 +1,2 @@
+/*Responsive Menu version 1.1, 26.11.2015, Chronym -> http://chronyms.ch / https://github.com/Chronyms/responsiveMenu*/
+function hoverDropDown(){$("nav ul li").hoverIntent(function(o){$(this).find(">ul").slideDown("fast")},function(o){$(this).find("ul").slideUp("fast")})}function clickDropDown(){$("li").click(function(){$("li > ul").not($(this).children("ul")).slideUp(),$(this).children("ul").slideToggle()})}$(document).ready(function(){jQuery.browser.mobile?(console.log("Mobile"),clickDropDown()):(console.log("Desktop"),hoverDropDown()),$("nav").addClass("js")});
